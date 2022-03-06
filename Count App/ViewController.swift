e238685bcfa079f99f48bb6878b34c3c3e521373
YochiyanXc <Kashima.Yochiyan@icloud.com>
2022-03-06 22:38:01 +0900

    とりま完成にする。

--- Count App/ViewController.swift
+++ Count App/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import AudioToolbox
+
+func vibrate() {
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    @IBAction func plus1() {
+        AudioServicesPlaySystemSound(1003);
+           AudioServicesDisposeSystemSoundID(1003);
+        number = number + 1
+        label.text = String(number)
+    }
+    @IBAction func plus2() {
+        AudioServicesPlaySystemSound(1003);
+           AudioServicesDisposeSystemSoundID(1003);
+number = number + 2
+        label.text = String(number)
+    }
+    @IBAction func mius1() {
+        AudioServicesPlaySystemSound(1003);
+           AudioServicesDisposeSystemSoundID(1003);
+number = number - 1
+        label.text =  String(number)
+    }
+    @IBAction func mius2() {
+        AudioServicesPlaySystemSound(1003);
+           AudioServicesDisposeSystemSoundID(1003);
+number = number - 2
+        label.text = String(number)
+    }
+    @IBAction func zero() {
+        number = 0
+        label.text = String(number)
+    }
 
 }
 
